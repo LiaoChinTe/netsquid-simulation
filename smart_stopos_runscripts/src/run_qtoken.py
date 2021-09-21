71ad175db8cc994df3bfdf41cd931c4bf5b919ad
ctliao <liao.chinte@gmail.com>
2021-09-21 18:32:50 +0200

    fix scale

--- smart_stopos_runscripts/src/run_qtoken.py
+++ smart_stopos_runscripts/src/run_qtoken.py
@@ -12,7 +12,7 @@ def myStepFunction(x):
     else:
         return 0 
 
-def myCostFunction(t1,t2,p1,p2,Srate,T,SrateMin=0.875,Tmin=10**9,w1=1,w2=0,w3=1,sf=myStepFunction
+def myCostFunction(t1,t2,p1,p2,Srate,T=0,SrateMin=0.875,Tmin=10**9,w1=1,w2=0,w3=1,sf=myStepFunction
     ,t1b=10,t2b=4.9,p1b=0.95,p2b=0.995):
     tmp1=w1*sf(SrateMin-Srate)
     tmp2=w2*sf(Tmin-T)
@@ -28,17 +28,18 @@ if __name__ == "__main__":
                         help="Quantum memory relaxation time (ns).")
     parser.add_argument('--T2', type=float,
                         help="Quantum memory dephasing time (ns).")
-    parser.add_argument('--wait_time', type=float,
-                        help="Alice's waiting time (ns).")
+    #parser.add_argument('--wait_time', type=float,
+    #                    help="Alice's waiting time (ns).")
     parser.add_argument('--filebasename', type=str,
                         help="Beginning of filename where results will be stored.")
 
     args = parser.parse_args()
-    mem_noise_model = T1T2NoiseModel(T1=(1/(1-args.T1))-1, T2=(1/(1-args.T2))-1, waitTime=args.wait_time)
-    res = run_QToken_sim(memNoiseModel=mem_noise_model, runTimes=2)
-    cost = myCostFunction(t1=(1/(1-args.T1))-1,t2=(1/(1-args.T2))-1,p1=0.95,p2=0.995,Srate=res,T=args.wait_time)
+    mem_noise_model = T1T2NoiseModel(T1=((1/(1-args.T1))-1)*3.6*10**12, T2=((1/(1-args.T2))-1)*10**6)
+    res = run_QToken_sim(memNoiseModel=mem_noise_model, runTimes=2,waitTime=10**9)
+    print("Srate==========================",res,"==========================================")
+    cost = myCostFunction(t1=(1/(1-args.T1))-1,t2=(1/(1-args.T2))-1,p1=0.95,p2=0.995,Srate=res)
 
-    df = pd.DataFrame(columns=["cost", "T1", "T2", "wait_time"])
-    df.loc[0] = [cost, args.T1, args.T2, args.wait_time]
+    df = pd.DataFrame(columns=["cost", "T1", "T2"])
+    df.loc[0] = [cost, args.T1, args.T2]
     csv_filename = args.filebasename + '.csv'
     df.to_csv(csv_filename, index=False, header=False)
