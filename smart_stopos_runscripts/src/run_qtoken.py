ffc70c38ffdf4de83d889ab4e980e48cec80911d
ctliao <liao.chinte@gmail.com>
2021-09-27 19:40:25 +0200

    fix log scale

--- smart_stopos_runscripts/src/run_qtoken.py
+++ smart_stopos_runscripts/src/run_qtoken.py
@@ -13,10 +13,11 @@ def myStepFunction(x):
         return 0 
 
 def myCostFunction(t1,t2,p1,p2,Srate,T=0,SrateMin=0.875,Tmin=10**9,w1=1,w2=0,w3=1,sf=myStepFunction
-    ,t1b=10,t2b=4.9,p1b=0.95,p2b=0.995):
+    ,t1b=0.9091 ,t2b=0.8305,p1b=0.95,p2b=0.995):
     tmp1=w1*sf(SrateMin-Srate)
     tmp2=w2*sf(Tmin-T)
-    C=1/(1+math.log(t1,t1b))+1/(1+math.log(t2,t2b))+1/(1+math.log(p1,p1b))+1/(1+math.log(p2,p2b))
+    #print("~~t1:",t1," t1b:",t1b)
+    C=1/(1+math.log(t1,t1b))+1/(1+math.log(t2,t2b)) #+1/(1+math.log(p1,p1b))+1/(1+math.log(p2,p2b))
     tmp3=w3*C
     
     return tmp1+tmp2+tmp3
@@ -36,8 +37,15 @@ if __name__ == "__main__":
     args = parser.parse_args()
     mem_noise_model = T1T2NoiseModel(T1=((1/(1-args.T1))-1)*3.6*10**12, T2=((1/(1-args.T2))-1)*10**6)
     res = run_QToken_sim(memNoiseModel=mem_noise_model, runTimes=2,waitTime=10**9)
-    print("Srate==========================",res,"==========================================")
-    cost = myCostFunction(t1=(1/(1-args.T1))-1,t2=(1/(1-args.T2))-1,p1=0.95,p2=0.995,Srate=res)
+
+    if res>0.875:
+        print("O Srate ","==========================",res,"==========================================")
+        
+    else:
+        print("X Srate ","==========================",res,"==========================================")
+
+
+    cost = myCostFunction(t1=args.T1,t2=args.T2,p1=0.95,p2=0.995,Srate=res) #(1/(1-args.T1))-1
 
     df = pd.DataFrame(columns=["cost", "T1", "T2"])
     df.loc[0] = [cost, args.T1, args.T2]
