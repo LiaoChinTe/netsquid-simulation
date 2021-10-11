40ae9e65d9ec5fb295046ead399ca57955e33d50
ctliao <liao.chinte@gmail.com>
2021-10-11 11:26:06 +0200

    fix cost function

--- smart_stopos_runscripts/src/run_qtoken.py
+++ smart_stopos_runscripts/src/run_qtoken.py
@@ -16,8 +16,7 @@ def myCostFunction(t1,t2,p1,p2,Srate,T=0,SrateMin=0.875,Tmin=10**9,w1=1,w2=0,w3=
     ,t1b=0.9091 ,t2b=0.8305,p1b=0.95,p2b=0.995):
     tmp1=w1*sf(SrateMin-Srate)
     tmp2=w2*sf(Tmin-T)
-    #print("~~t1:",t1," t1b:",t1b)
-    C=1/(1+math.log(t1,t1b))+1/(1+math.log(t2,t2b)) #+1/(1+math.log(p1,p1b))+1/(1+math.log(p2,p2b))
+    C=1/math.log(t1,t1b)+1/math.log(t2,t2b) #+1/(1+math.log(p1,p1b))+1/(1+math.log(p2,p2b))
     tmp3=w3*C
     
     return tmp1+tmp2+tmp3
@@ -38,12 +37,13 @@ if __name__ == "__main__":
     mem_noise_model = T1T2NoiseModel(T1=((1/(1-args.T1))-1)*3.6*10**12, T2=((1/(1-args.T2))-1)*10**6)
     res = run_QToken_sim(memNoiseModel=mem_noise_model, runTimes=2,waitTime=10**9)
 
+    '''
     if res>0.875:
         print("O Srate ","==========================",res,"==========================================")
         
     else:
         print("X Srate ","==========================",res,"==========================================")
-
+    '''
 
     cost = myCostFunction(t1=args.T1,t2=args.T2,p1=0.95,p2=0.995,Srate=res) #(1/(1-args.T1))-1
 
