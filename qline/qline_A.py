0fcd535c70e1a7a04653b69cfec211e9c12c7837
ctliao <liao.chinte@gmail.com>
2022-01-06 15:56:31 +0100

    dummy test

--- qline/qline_A.py
+++ qline/qline_A.py
@@ -1,3 +1,4 @@
+from netsquid.components.qprogram import QuantumProgram
 from netsquid.protocols import NodeProtocol
 from netsquid.components import QSource,Clock
 from netsquid.components.qsource import SourceStatus
@@ -9,20 +10,69 @@ sys.path.append(scriptpath)
 from functions import *
 
 
+
+class Dummy(QuantumProgram):
+    def __init__(self):
+        super().__init__()
+
+    def program(self):
+
+        yield self.run(parallel=False)
+
+
+
+
 class AliceProtocol(NodeProtocol):
-    def __init__(self,node,processor,num_bits=10):
+    def __init__(self,node,processor,num_bits=10,source_frq=1e9):
         super().__init__()
         
         self.node=node
         self.processor=processor
         self.num_bits=num_bits
+        self.source_frq=source_frq
+
+        self.sourceQList=[]
 
         #generat qubits from source
-        self.A_Source = QSource("Alice_source"
-            ,status=SourceStatus.EXTERNAL) # enable frequency
-        #self.A_Source.ports["qout0"].bind_output_handler(self.storeSourceOutput)
+        self.A_Source = QSource("Alice_source",status=SourceStatus.EXTERNAL) # enable frequency
+        self.A_Source.ports["qout0"].bind_output_handler(self.storeSourceOutput)
+
+        #forward_output(self.node.ports["portQO"])
+        #bind_output_handler(self.storeSourceOutput)
+
 
     def run(self):
         print("AliceProtocol running")
 
+        self.A_genQubits(num_bits=self.num_bits,freq=self.source_frq)
+        
+        # wait
+        yield self.await_program(processor=self.processor)
+
+        print("qubits ready")
+
+
+    def A_genQubits(self,num_bits,freq=1e9):
+        #set clock
+        print("in A_genQubits")
+        clock = Clock("clock", frequency=freq, max_ticks=num_bits)
+        print("num_bits:",num_bits)
+        try:
+            
+            clock.ports["cout"].connect(self.A_Source.ports["trigger"])
+        except:
+            pass
+        #print("ticking")
+        clock.start()
+
+    # for Alice to store qubits in qmem
+    def storeSourceOutput(self,qubit):
+
+        self.sourceQList.append(qubit.items[0])
+        if len(self.sourceQList)==self.num_bits:
+            print("qubit stored")
+            self.processor.put(qubits=self.sourceQList)
+            myDummy=Dummy()
+            self.processor.execute_program(myDummy)
+    
 
--- qline/qline_main.py
+++ qline/qline_main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import netsquid as ns
 from netsquid.nodes.node import Node
-from netsquid.protocols import Protocol,LocalProtocol
+from netsquid.protocols import LocalProtocol
 from netsquid.qubits import create_qubits
 from netsquid.qubits.operators import X,H,Z
 from netsquid.nodes.connections import DirectConnection
@@ -23,9 +23,9 @@ import qline_B
 import qline_C
 
 
-def createProcessor_QL(processorName,momNoise=None,gateNoice=None):
+def createProcessor_QL(processorName,capacity=20,momNoise=None,gateNoice=None):
 
-    myProcessor=QuantumProcessor(processorName, num_positions=2,
+    myProcessor=QuantumProcessor(processorName, num_positions=capacity,
         mem_noise_models=momNoise, phys_instructions=[
             PhysicalInstruction(INSTR_X, duration=10, quantum_noise_model=gateNoice),
             PhysicalInstruction(INSTR_H, duration=10, quantum_noise_model=gateNoice),
@@ -40,35 +40,39 @@ def run_QLine_sim(numNode=3,fibreLen=10,qdelay=0,cdelay=0):
     ns.sim_reset()
     NodeList=[]
     ProcessorList=[]
+    myCharlieProtocolList=[]
+
     for i in range(numNode):
 
-        # create nodes
+        # create nodes===========================================================
         tmpNode=Node("Node_"+str(i), port_names=["portQI","portQO","portCI","portCO"]) # quantum/classical input/output
         NodeList.append(tmpNode)
 
-        # create processor
-        ProcessorList.append(createProcessor_QL(processorName="Processor_"+str(i)))
+        # create processor===========================================================
+        ProcessorList.append(createProcessor_QL(processorName="Processor_"+str(i),momNoise=None,gateNoice=None))
 
 
-        # channels==================================================================
         
-        MyQChannel=QuantumChannel("QChannel_forward_"+str(i),delay=qdelay,length=fibreLen
-            ,models={"myFibreLossModel": FibreLossModel(p_loss_init=0.2, p_loss_length=0.25, rng=None)})
-        
-        ## connections
+        # channels in between nodes==============================================================
         if i>0:
+
+            # Q channels==================================================================
+        
+            MyQChannel=QuantumChannel("QChannel_forward_"+str(i),delay=qdelay,length=fibreLen
+                ,models={"myFibreLossModel": FibreLossModel(p_loss_init=0.2, p_loss_length=0.25, rng=None)})
+
             NodeList[i-1].connect_to(NodeList[i], MyQChannel,
                 local_port_name =NodeList[i-1].ports["portQO"].name,
                 remote_port_name=NodeList[i].ports["portQI"].name)
-        
 
+            # C channals==================================================================
             MyCChannel_F = ClassicalChannel("CChannel_forward_"+str(i),delay=cdelay,length=fibreLen)
             MyCChannel_B = ClassicalChannel("CChannel_backward_"+str(i),delay=cdelay,length=fibreLen)
 
             myDirectConnection=DirectConnection("DConnection_forward_"+str(i),channel_AtoB=MyCChannel_F,channel_BtoA=MyCChannel_B)
         
             NodeList[i-1].connect_to(NodeList[i], myDirectConnection,local_port_name="portCO", remote_port_name="portCI")
-            
+    
 
 
 
@@ -77,13 +81,18 @@ def run_QLine_sim(numNode=3,fibreLen=10,qdelay=0,cdelay=0):
 
         # assign Charlie protocol
         if i!=0 and i!=numNode-1:
-            myCharlieProtocol=qline_C.CharlieProtocol(node=NodeList[i],processor=ProcessorList[i])
+            myCharlieProtocolList.append(qline_C.CharlieProtocol(node=NodeList[i],processor=ProcessorList[i])) 
 
 
     myAliceProtocol=qline_A.AliceProtocol(node=NodeList[0],processor=ProcessorList[0])
     myBobProtocol=qline_B.BobProtocol(node=NodeList[-1],processor=ProcessorList[-1])
     
 
+    myBobProtocol.start()
+    for Charlie in myCharlieProtocolList:
+        Charlie.start()
+
+    myAliceProtocol.start()
 
     ns.sim_run()
 
